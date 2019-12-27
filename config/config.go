14f4064fd71cad58e4376c7d53e362b478a90136
meowovo <meowovo@github.com>
2019-12-27 23:46:39 +0800

    初始化数据库连接

--- config/config.go
+++ config/config.go
@@ -18,5 +18,15 @@ func LoadBaseConfig() {
 
 //端口号
 func GetPort() string {
-	return viper.GetString("port")
+	return viper.GetString("main_port")
+}
+
+func GetMySQLConfig() string {
+	host := viper.GetString("host")
+	mysqlUser := viper.GetString("mysql_user")
+	mysqlPassword := viper.GetString("mysql_password")
+	mysqlPort := viper.GetString("mysql_port")
+	databaseName := viper.GetString("database_name")
+	mysqlConfig := mysqlUser + ":" + mysqlPassword + "@tcp(" + host + mysqlPort + ")" + databaseName
+	return mysqlConfig
 }
--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"Blog/config"
+	"database/sql"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func main() {
+
+	config.LoadBaseConfig()
+
+	db, err := sql.Open("mysql", config.GetMySQLConfig())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
 	router := gin.Default()
 	gin.SetMode(gin.DebugMode)
 	router.GET("/", func(c *gin.Context) {
