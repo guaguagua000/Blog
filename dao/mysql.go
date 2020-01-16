620134e220ef64db378b398172f630b98b562f89
meowovo <baozhe.nie@meowovo.com>
2020-01-16 11:33:49 +0800

    mysql charset and time.Time support

--- dao/mysql.go
+++ dao/mysql.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func Init() {
 	var err error
-	mysqlURL := strings.Join([]string{config.GetString("mysql.user"), ":", config.GetString("mysql.password"), "@", config.GetString("mysql.protocol"), "(", config.GetString("mysql.host"), ":", config.GetString("mysql.port"), ")/", config.GetString("mysql.database")}, "")
+	mysqlURL := strings.Join([]string{config.GetString("mysql.user"), ":", config.GetString("mysql.password"), "@", config.GetString("mysql.protocol"), "(", config.GetString("mysql.host"), ":", config.GetString("mysql.port"), ")/", config.GetString("mysql.database"), "?", config.GetString("mysql.charset"), "&", config.GetString("mysql.timeSupport")}, "")
 	DB, err := gorm.Open("mysql", mysqlURL)
 	if err != nil {
 		log.Panicln("--Init MySQL Connection Error:", err, "--")
