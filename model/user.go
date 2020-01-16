c13d1c40bbc15afba925cc76c04f0b68cae16004
meowovo <baozhe.nie@meowovo.com>
2020-01-16 11:53:55 +0800

    user struct

--- model/user.go
+++ model/user.go
@@ -6,10 +6,10 @@ import "database/sql"
 用户表结构体
 */
 type User struct {
-	id       int64         `gorm:"PRIMARY_KEY;AUTO_INCREMENT;NOT NULL"`
-	username string        `gorm:"Type:varchar(64);NOT NULL"`
-	password string        `gorm:"Type:varchar(64);NOT NULL"`
-	nickname string        `gorm:"Type:varchar(64);NOT NULL"`
-	avatar   string        `gorm:"Type:varchar(64);NOT NULL;DEFAULT:'public/img/default_avatar.png'"`
-	sex      sql.NullInt32 `gorm:"Type:INT(1);"`
+	Id       int64         `gorm:"PRIMARY_KEY;AUTO_INCREMENT;NOT NULL"`
+	Username string        `gorm:"Type:varchar(64);NOT NULL"`
+	Password string        `gorm:"Type:varchar(64);NOT NULL"`
+	Nickname string        `gorm:"Type:varchar(64);NOT NULL"`
+	Avatar   string        `gorm:"Type:varchar(64);NOT NULL;DEFAULT:'public/img/default_avatar.png'"`
+	Sex      sql.NullInt32 `gorm:"Type:INT(1);"`
 }
